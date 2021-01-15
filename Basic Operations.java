0e7b9cc41937e5e2f91736acb9b95eff000d74e4
Sahil Tripathi <55251741+sahil2128@users.noreply.github.com>
2021-01-16 00:52:02 +0530

    Create Basic Operations.java

---
+++ Basic Operations.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int num1 = 6;
+        int num2 = 12;
+        int num3 = 24;
+        int sum = num1+num2+num3;
+        System.out.print("the sum is = ");
+        System.out.println(sum);
+    }
+}
