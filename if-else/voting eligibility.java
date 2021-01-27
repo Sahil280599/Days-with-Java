283ad2b0642b1cf66ca40b2ec39efa1dcf566086
Sahil Tripathi <55251741+sahil2128@users.noreply.github.com>
2021-01-27 22:55:26 +0530

    Update voting eligibility.java

--- if-else/voting eligibility.java
+++ if-else/voting eligibility.java
@@ -1,10 +1,12 @@
-package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int age = 20;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the age=");
+        int age=sc.nextInt();
         if (age > 18) {
             System.out.print("age is 18 eligible to vote");
         } else {
