9a3dde187d3c8307b0d5ee5f3aa576963f7cb99a
Sahil Tripathi <55251741+sahil2128@users.noreply.github.com>
2021-01-27 22:49:17 +0530

    Create voting eligibility.java

---
+++ if-else/voting eligibility.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int age = 20;
+        if (age > 18) {
+            System.out.print("age is 18 eligible to vote");
+        } else {
+            System.out.print("false");
+        }
+    }
+}
