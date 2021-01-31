250dcce0b67467af28ac2dc7d472166c51c3889d
Sahil Tripathi <55251741+sahil2128@users.noreply.github.com>
2021-01-31 20:21:00 +0530

    Create Not return.java

---
+++ Method Overloading/Not return.java
@@ -0,0 +1,15 @@
+package com.company;
+
+// if we dont want anyrthing return 
+public class Method_overloading {
+    static void telljoke(){
+        System.out.println("fuck");
+
+    }
+
+    public static void main(String[] args) {
+        telljoke();
+
+    }
+}
+
