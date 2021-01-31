40acf87bbc2d3ad554b67b0698eab52f9aec9606
Sahil Tripathi <55251741+sahil2128@users.noreply.github.com>
2021-01-31 17:59:12 +0530

    Create Print multiplication table.java

---
+++ For-loop/Print multiplication table.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number = ");
+        int a = sc.nextInt();
+        {
+            for (int i = 0; i <= 10; i++)
+                System.out.println(a * i);
+        }
+    }
+}
+
+
+
+
+
+
+
+
